f40e99ed18410c4a3e1deb41d4ede984d83f7b00
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-11 18:22:25 +0500

    delete ND UPDATE routes added

--- Day 6/node_app/controllers/test.js
+++ Day 6/node_app/controllers/test.js
@@ -6,7 +6,7 @@ const printHelloWorld = (req, res) => {
     res.send('\n hello world')
 };
 
-const getUsers = async(req, res) => {
+const getAllUsers = async(req, res) => {
   try{
     users = await UserModel.findAll({
         include: [{
@@ -17,13 +17,29 @@ const getUsers = async(req, res) => {
     res.send(users);
   }
   catch(e){
-    if (e.name === 'SequelizeValidationError') {
-      return res.status(400).json({
-        success: false,
-        msg: e.errors.map(err => err.message)
-      })
+    if(!e.status) {
+      res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+    } else {
+        res.status(e.status).json( { error: { code: e.code, message: e.message } });
     }
-    else if(!e.status) {
+  }
+}
+
+const getUser = async(req, res) => {
+  try{
+    const user = await UserModel.findOne({
+      where: {
+        id: req.params.user_id
+      }
+    });
+    if (user === null) {
+      res.send('user not found against that id');
+    }
+    else
+      res.send(user);
+  }
+  catch(e){
+    if(!e.status) {
       res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
     } else {
         res.status(e.status).json( { error: { code: e.code, message: e.message } });
@@ -56,9 +72,54 @@ const createUser = async(req, res) => {
   }
 }
 
+const updateUser = async(req, res) => {
+  try{
+    let object = {};
+    if(req.body.firstName!=null)  object.firstName = req.body.firstName;
+    if(req.body.lastName!=null)  object.lastName = req.body.lastName;
+    if(req.body.email!=null)  object.email = req.body.email;
+    if(req.body.password!=null)  object.password = req.body.password;
+    console.log(object);
+    UserModel.update(object,{
+      where: {
+        id: req.body.user_id
+      }
+    });
+    res.send('User updated successfully');
+  }
+  catch(e){
+    if(!e.status) {
+      res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+    } else {
+        res.status(e.status).json( { error: { code: e.code, message: e.message } });
+    }
+  }
+}
+
+const deleteUser = async(req, res) => {
+  try{
+    UserModel.destroy({
+      where: {
+        id: req.body.user_id
+      }
+    });
+    res.send('User deleted successfully');
+  }
+  catch(e){
+    if(!e.status) {
+      res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+    } else {
+        res.status(e.status).json( { error: { code: e.code, message: e.message } });
+    }
+  }
+}
+
 
 module.exports = {
     printHelloWorld,
-    getUsers,
-    createUser
+    getAllUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
 };
--- Day 6/node_app/routes/using_router.js
+++ Day 6/node_app/routes/using_router.js
@@ -5,8 +5,10 @@ const testController = require('../controllers/test');
 
 // define the about route
 router.post('/', middleware.timeLog, testController.printHelloWorld);
-router.get('/getAll', testController.getUsers);
+router.get('/getAll', testController.getAllUsers);
+router.get('/getOne/:user_id', testController.getUser);
 router.post('/create', testController.createUser);
-
+router.post('/delete', testController.deleteUser);
+router.post('/update', testController.updateUser);
 
 module.exports = router
