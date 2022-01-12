7fbbdb591a2d439c964a338e8aea8cfb890e39f5
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-12 09:51:00 +0500

    port changed from 3000 to 8000

--- Day 6/node_app/index.js
+++ Day 6/node_app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var userRoutes = require('./routes/user');
 const app = express();
-const port = 3000;
+const port = 8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
