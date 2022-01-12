d0be3cbff952b9447e1dbc8ba9187f2720a10ad4
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-12 11:22:03 +0500

    try catch added on middleware

--- Day 6/node_app/middlewares/user.js
+++ Day 6/node_app/middlewares/user.js
@@ -11,19 +11,28 @@ const timeLog = (req, res, next) => {
 
 
 const validateUserCreate = (req, res, next) => {
-  const schema = Joi.object().keys({
-    first_name: Joi.string().regex(/^[a-zA-Z]+$/).min(3).max(30).required(),
-    last_name: Joi.string().regex(/^[a-zA-Z]+$/).min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(5).max(30).required()
-  });
-  const result = schema.validate(req.body); 
-  if(result.error == null)  //means valid
-    next();
-  else
-    return res.status(400).json({
-    success: false,
-    msg: result.error.details.map(i => i.message).join(',')})
+  try{
+      const schema = Joi.object().keys({
+        first_name: Joi.string().regex(/^[a-zA-Z]+$/).min(3).max(30).required(),
+        last_name: Joi.string().regex(/^[a-zA-Z]+$/).min(3).max(30).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(5).max(30).required()
+      });
+      const result = schema.validate(req.body); 
+      if(result.error == null)  //means valid
+        next();
+      else
+        return res.status(400).json({
+        success: false,
+        msg: result.error.details.map(i => i.message).join(',')})
+  }
+  catch(e){
+      if(!e.status) {
+        res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+      } else {
+          res.status(e.status).json( { error: { code: e.code, message: e.message } });
+      }
+  }
 }
 
 module.exports = {
