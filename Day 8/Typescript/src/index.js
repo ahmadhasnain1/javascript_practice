c12270413421d57293348edcda1b36df486326d7
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-13 16:41:01 +0500

    typescript practice added in day 8

---
+++ Day 8/Typescript/src/index.js
@@ -0,0 +1,90 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var message = 'hello world';
+console.log(message);
+var name = 'Ahmad';
+var isBeginner = true;
+var sentence = "My name is ".concat(name, "\nI am a begineer in Typescript");
+console.log(sentence);
+var n = null;
+var u = undefined;
+// let isNew : boolean = null;
+// let myName:string = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Ahmad', 24];
+var anyType;
+anyType = 20;
+anyType = "abc";
+var multiType;
+multiType = 12;
+multiType = true;
+function add(num1, num2) {
+    return num1 + num2;
+}
+function add2(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+function add3(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    return num1 + num2;
+}
+add(1, 2);
+add2(5);
+add3(5);
+function fullName(person) {
+    console.log("".concat(person.firstName, " ").concat(person.lastName));
+}
+function fullName2(person) {
+    console.log("".concat(person.firstName, " ").concat(person.lastName));
+}
+var p = {
+    firstName: "Ahmad",
+    lastName: "Hasnain"
+};
+fullName(p);
+fullName2(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning ' ".concat(this.employeeName));
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Ahmad');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Saud');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
---
+++ Day 8/Typescript/src/index.ts
@@ -0,0 +1,103 @@
+export {}
+let message = 'hello world';
+console.log(message);
+
+let name: string = 'Ahmad';
+let isBeginner : boolean = true;
+let sentence: string = `My name is ${name}
+I am a begineer in Typescript`;
+
+console.log(sentence);
+
+let  n:null = null;
+let u:undefined = undefined;
+
+// let isNew : boolean = null;
+// let myName:string = undefined;
+
+let list1 :number[] = [1,2,3];
+let list2 : Array<number> = [1,2,3];
+
+let person1 :[string,number] = ['Ahmad', 24];
+
+let anyType : any ;
+anyType = 20;
+anyType = "abc";
+
+let multiType : number|boolean;
+multiType = 12;
+multiType = true;
+
+function add (num1:number, num2:number) : number {
+    return num1+num2;
+}
+
+function add2 (num1:number, num2?:number) : number {  //optional param
+    if(num2)
+        return num1+num2;
+    else
+        return num1;
+}
+
+function add3 (num1:number, num2:number = 10) : number {  //default value of optional param
+        return num1+num2;
+}
+
+add(1,2);
+add2(5);
+add3(5); 
+
+interface Person {
+    firstName:string,
+    lastName:string
+}
+
+function fullName(person : {firstName:string, lastName:string}){  //without using interface
+    console.log(`${person.firstName} ${person.lastName}`);
+}
+
+function fullName2(person : Person){  //using interface
+    console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p={
+    firstName: "Ahmad",
+    lastName: "Hasnain"
+}
+
+fullName(p);
+
+fullName2(p);
+
+
+class Employee {   //class
+    public employeeName: string  //access modifier is public
+
+    constructor(name:string){
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good morning ' ${this.employeeName}`);
+    }
+
+}
+
+let emp1 = new Employee('Ahmad');
+console.log(emp1.employeeName);
+emp1.greet();
+
+
+class Manager extends Employee{   //inheritance
+    constructor(managerName:string){
+        super(managerName);
+    }
+    delegateWork(){
+         console.log(`Manager delegating tasks`);
+    }
+}
+
+let m1 = new Manager('Saud');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
